7155d96fde7f7c492a06ddc199cb3eb6f6b9b0a4
Jan Marshal <jan@marshalcode.com>
2025-12-04 19:21:54 +0100

    small-fix

--- app/(shared-layout)/blog/page.tsx
+++ app/(shared-layout)/blog/page.tsx
@@ -4,9 +4,9 @@ import { Skeleton } from "@/components/ui/skeleton";
 import { api } from "@/convex/_generated/api";
 import { fetchQuery } from "convex/nextjs";
 import { Metadata } from "next";
-import { cacheLife, cacheTag } from "next/cache";
 import Image from "next/image";
 import Link from "next/link";
+import { connection } from "next/server";
 import { Suspense } from "react";
 
 export const metadata: Metadata = {
@@ -36,9 +36,10 @@ export default function BlogPage() {
 }
 
 async function LoadBlogList() {
-  "use cache";
+  /* "use cache";
   cacheLife("hours");
-  cacheTag("blog");
+  cacheTag("blog"); */
+  await connection();
   const data = await fetchQuery(api.posts.getPosts);
 
   return (
