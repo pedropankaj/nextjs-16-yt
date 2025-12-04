ab448f3ec03d72ac3dd4a7dcd5c9c9fdf40bb801
Jan Marshal <jan@marshalcode.com>
2025-12-04 18:31:16 +0100

    finished

---
+++ app/(shared-layout)/blog/page.tsx
@@ -0,0 +1,100 @@
+import { buttonVariants } from "@/components/ui/button";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
+import { api } from "@/convex/_generated/api";
+import { fetchQuery } from "convex/nextjs";
+import { Metadata } from "next";
+import { cacheLife, cacheTag } from "next/cache";
+import Image from "next/image";
+import Link from "next/link";
+import { connection } from "next/server";
+import { Suspense } from "react";
+
+export const metadata: Metadata = {
+  title: "Blog | Next.js 16 Tutorial",
+  description: "Read our latest articles and insights.",
+  category: "Web development",
+  authors: [{ name: "Jan marshal" }],
+};
+
+export default function BlogPage() {
+  return (
+    <div className="py-12">
+      <div className="text-center pb-12">
+        <h1 className="text-4xl font-extrabold tracking-tight sm:text-5xl">
+          Our Blog
+        </h1>
+        <p className="pt-4 max-w-2xl mx-auto text-xl text-muted-foreground">
+          Insights, thoughts, and trends from our team.
+        </p>
+      </div>
+
+      {/* <Suspense fallback={<SkeletonLoadingUi />}> */}
+      <LoadBlogList />
+      {/*    </Suspense> */}
+    </div>
+  );
+}
+
+async function LoadBlogList() {
+  "use cache";
+  cacheLife("hours");
+  cacheTag("blog");
+  const data = await fetchQuery(api.posts.getPosts);
+
+  return (
+    <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+      {data?.map((post) => (
+        <Card key={post._id} className="pt-0">
+          <div className="relative h-48 w-full overflow-hidden">
+            <Image
+              src={
+                post.imageUrl ??
+                "https://images.unsplash.com/photo-1761019646782-4bc46ba43fe9?q=80&w=1631&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              }
+              alt="image"
+              fill
+              className="rounded-t-lg object-cover"
+            />
+          </div>
+
+          <CardContent>
+            <Link href={`/blog/${post._id}`}>
+              <h1 className="text-2xl font-bold hover:text-primary">
+                {post.title}
+              </h1>
+            </Link>
+            <p className="text-muted-foreground line-clamp-3">{post.body}</p>
+          </CardContent>
+          <CardFooter>
+            <Link
+              className={buttonVariants({
+                className: "w-full",
+              })}
+              href={`/blog/${post._id}`}
+            >
+              Read more
+            </Link>
+          </CardFooter>
+        </Card>
+      ))}
+    </div>
+  );
+}
+
+function SkeletonLoadingUi() {
+  return (
+    <div className="grid gap-6 md:grid-cols-3 lg:grid-cols-3">
+      {[...Array(3)].map((_, i) => (
+        <div className="flex flex-col space-y-3" key={i}>
+          <Skeleton className="h-48 w-full rounded-xl" />
+          <div className="space-y-2 flex flex-col">
+            <Skeleton className="h-6 w-3/4" />
+            <Skeleton className="h-4 w-full" />
+            <Skeleton className="h-4 w-2/" />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  cacheComponents: true,
+  images: {
+    remotePatterns: [
+      {
+        hostname: "images.unsplash.com",
+        protocol: "https",
+        port: "",
+      },
+      {
+        hostname: "lovable-bloodhound-237.convex.cloud",
+        protocol: "https",
+        port: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
